d43c2ea49569c44faab636c3c7a7547363c7754b
armagadon159753 <armagadon159753@gmail.com>
2017-06-14 15:48:37 +0200

    Fix unread bit

--- src/pocketmine/network/protocol/EntityFallPacket.php
+++ src/pocketmine/network/protocol/EntityFallPacket.php
@@ -24,16 +24,29 @@ namespace pocketmine\network\protocol;
 #include <rules/DataPacket.h>
 
 
-class EntityFallPacket extends DataPacket {
+use pocketmine\network\mcpe\NetworkSession;
+
+class EntityFallPacket extends DataPacket{
 	const NETWORK_ID = Info::ENTITY_FALL_PACKET;
 
+	public $entityRuntimeId;
 	public $fallDistance;
+	public $bool1;
 
 	public function decode(){
+		$this->entityRuntimeId = $this->getEntityRuntimeId();
 		$this->fallDistance = $this->getLFloat();
+		$this->bool1 = $this->getBool();
 	}
 
 	public function encode(){
+		$this->reset();
+		$this->putEntityRuntimeId($this->entityRuntimeId);
+		$this->putLFloat($this->fallDistance);
+		$this->putBool($this->bool1);
+	}
 
+	public function handle(NetworkSession $session) : bool{
+		return $session->handleEntityFall($this);
 	}
 }
