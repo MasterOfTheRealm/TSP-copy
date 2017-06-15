4287fbbf240a606fa85edd84874c0277150aa3a9
Rateek <rateek34@gmail.com>
2017-06-15 14:12:07 +0200

    fix lol

--- src/pocketmine/network/protocol/RespawnPacket.php
+++ src/pocketmine/network/protocol/RespawnPacket.php
@@ -25,12 +25,9 @@ namespace pocketmine\network\protocol;
 
 #include <rules/DataPacket.h>
 
-
-use pocketmine\network\protocol\NetworkSession;
-
-class RespawnPacket extends DataPacket{
+class RespawnPacket extends DataPacket {
+	
 	const NETWORK_ID = Info::RESPAWN_PACKET;
-
 	public $x;
 	public $y;
 	public $z;
@@ -47,5 +44,4 @@ class RespawnPacket extends DataPacket{
 	public function handle(NetworkSession $session) : bool{
 		return $session->handleRespawn($this);
 	}
-
 }
