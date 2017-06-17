3ee2548436eede02881f1660a0ceba113057361b
armagadon159753 <armagadon159753@gmail.com>
2017-06-17 18:59:48 +0200

    Crash server by AddEntityPacket solved

--- src/pocketmine/network/protocol/AddEntityPacket.php
+++ src/pocketmine/network/protocol/AddEntityPacket.php
@@ -23,54 +23,60 @@ namespace pocketmine\network\protocol;
 
 #include <rules/DataPacket.h>
 
-#ifndef COMPILE
 use pocketmine\entity\Attribute;
 
-#endif
+class AddEntityPacket extends DataPacket{
 
-class AddEntityPacket extends DataPacket {
-
-	const NETWORK_ID = Info::ADD_ENTITY_PACKET;
-
-	public $eid;
+	public $entityUniqueId = null; //TODO
+	public $entityRuntimeId;
 	public $type;
 	public $x;
 	public $y;
 	public $z;
-	public $speedX;
-	public $speedY;
-	public $speedZ;
-	public $yaw;
-	public $pitch;
+	public $speedX = 0.0;
+	public $speedY = 0.0;
+	public $speedZ = 0.0;
+	public $yaw = 0.0;
+	public $pitch = 0.0;
 	/** @var Attribute[] */
 	public $attributes = [];
 	public $metadata = [];
 	public $links = [];
 
 	public function decode(){
-
+		$this->entityUniqueId = $this->getEntityUniqueId();
+		$this->entityRuntimeId = $this->getEntityRuntimeId();
+		$this->type = $this->getUnsignedVarInt();
+		$this->getVector3f($this->x, $this->y, $this->z);
+		$this->getVector3f($this->speedX, $this->speedY, $this->speedZ);
+		$this->pitch = $this->getLFloat();
+		$this->yaw = $this->getLFloat();
+		$this->attributes = $this->getAttributeList();
+		$this->metadata = $this->getEntityMetadata();
+		$linkCount = $this->getUnsignedVarInt();
+		for($i = 0; $i < $linkCount; ++$i){
+			$this->links[$i][0] = $this->getEntityUniqueId();
+			$this->links[$i][1] = $this->getEntityUniqueId();
+			$this->links[$i][2] = $this->getByte();
+		}
 	}
 
-	$this->reset(); 
-	$this->putEntityId($this->eid); 
-	$this->putEntityId($this->eid); 
-	$this->putUnsignedVarInt($this->type); 
-	$this->putVector3f($this->x, $this->y, $this->z); 
-	$this->putVector3f($this->speedX, $this->speedY, $this->speedZ); 
-	$this->putLFloat($this->pitch); $this->putLFloat($this->yaw); 
-	$this->putUnsignedVarInt(count($this->attributes)); 
-	foreach($this->attributes as $entry){ 
-		$this->putString($entry->getName()); 
-		$this->putLFloat($entry->getMinValue()); 
-		$this->putLFloat($entry->getValue()); 
-		$this->putLFloat($entry->getMaxValue());
-	} 
-	$this->putEntityMetadata($this->metadata); 
-	$this->putUnsignedVarInt(count($this->links)); 
-	foreach($this->links as $link){
-		$this->putEntityUniqueId($link[0]);
-		$this->putEntityUniqueId($link[1]);
-		$this->putByte($link[2]); } } 
+	public function encode(){
+		$this->reset();
+		$this->putEntityUniqueId($this->entityUniqueId ?? $this->entityRuntimeId);
+		$this->putEntityRuntimeId($this->entityRuntimeId);
+		$this->putUnsignedVarInt($this->type);
+		$this->putVector3f($this->x, $this->y, $this->z);
+		$this->putVector3f($this->speedX, $this->speedY, $this->speedZ);
+		$this->putLFloat($this->pitch);
+		$this->putLFloat($this->yaw);
+		$this->putAttributeList(...$this->attributes);
+		$this->putEntityMetadata($this->metadata);
+		$this->putUnsignedVarInt(count($this->links));
+		foreach($this->links as $link){
+			$this->putEntityUniqueId($link[0]);
+			$this->putEntityUniqueId($link[1]);
+			$this->putByte($link[2]);
 		}
 	}
 }
